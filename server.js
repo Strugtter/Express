8bf78f1978176de5bb5cb8e4c1bb836b64de2a33
Camilo Botina Trujillo <camilobotinatrujillo@gmail.com>
2023-05-24 13:35:32 +1000

    Add config Mongo

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 import express from 'express';
+import 'dotenv/config.js';
+import './config/database.js';
 
 const server = express();
-
 const PORT = process.env.PORT || 8080;
-
 const ready = () => console.log("server ready " + PORT);
 
+//Middlewares
+server.use(express.json());
+server.use(express.urlencoded({extended :false}))
+//Router more server
 server.listen(PORT, ready);
-
